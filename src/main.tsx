a708136d5fb6362de6bdf885db138152d880b333
ClockWera <mikheevalex2k17@yandex.ru>
2023-04-02 15:04:25 +0300

    modified files

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,12 @@
-<<<<<<< HEAD:vite-project/src/main.tsx
+import { createRoot } from 'react-dom/client';
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {Body} from './Body'
-import {Header} from './Header'
+import { Body } from './Body'
+import { Header } from './Header'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Header />
     <Body />
   </React.StrictMode>,
-=======
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import BodyTS from './BodyTS'
-import HeaderTS from './HeaderTS'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <HeaderTS />
-    <BodyTS />
-  </React.StrictMode>,
->>>>>>> 5dafb63b0f827df0435e9881084f2f65bc3d6fee:src/main.tsx
 )
