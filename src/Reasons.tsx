68fd947a4c9ddd4e6f44dfc98bec666880dd1c9d
ClockWera <mikheevalex2k17@yandex.ru>
2023-05-03 15:33:40 +0300

    Пересобрал колонки в файле Reasons.css добавил классы к текстам убарал в AboutUs.css шрифт

--- src/Reasons.tsx
+++ src/Reasons.tsx
@@ -7,21 +7,21 @@ export const Reasons = () => {
         <div className="reputation__page">
             <h1 className="rep">Our Reputation</h1>
             <div className="three__columns">
-                <ul className="Services">
+                <li className="Services">
                     <img src={headphones} alt="headphones__logo" />
-                    <p>Best Services</p>
-                    <p>Nullam senectus porttitor in <br></br> eget. Eget rutrum leo interdum.</p>
-                </ul>
-                <ul className="Teams">
+                    <p className='Capital'>Best Services</p>
+                    <p className='under__text'>Nullam senectus porttitor in eget. Eget rutrum leo interdum.</p>
+                </li>
+                <li className="Teams">
                     <img src={headphones} alt="headphones__logo" />
-                    <p>Best Teams</p>
-                    <p>Cursus semper tellus volutpat <br></br> aliquet lacus.</p>
-                </ul>
-                <ul className="Designs">
+                    <p className='Capital'>Best Teams</p>
+                    <p className='under__text'>Cursus semper tellus volutpat aliquet lacus.</p>
+                </li>
+                <li className="Designs">
                     <img src={pencil} alt="pencil__logo" />
-                    <p>Best Designs</p>
-                    <p>Ultricies at ipsum nunc,<br></br> tristique nam lectus.</p>
-                </ul>
+                    <p className='Capital'>Best Designs</p>
+                    <p className='under__text'>Ultricies at ipsum nunc, tristique nam lectus.</p>
+                </li>
             </div>
         </div>
     )
